05c1c4244a21d05e844aacd6988a7ac285706b8c
SunStake <sunstake@protonmail.com>
2020-10-03 15:12:15 +0000

    Add basic config options

--- entry.sh
+++ entry.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 
-exec java -jar /usr/local/tron/FullNode.jar -c /etc/tron/main_net_config.conf -d /data
+CONFIG_FILE=/etc/tron/main_net_config.conf
+CONFIG_P2P_PORT=18888
+CONFIG_FULL_NODE_PORT=8090
+CONFIG_SOLIDITY_NODE_PORT=8091
+
+if [[ -z "${NETWORK}" ]] || [[ "${NETWORK}" == "mainnet" ]]; then
+  # Network set to mainnet
+  :
+elif [[ "${NETWORK}" == "testnet" ]]; then
+  # Network set to testnet
+  CONFIG_FILE=/etc/tron/test_net_config.conf
+  :
+else
+  echo "Invalid NETWORK: ${NETWORK}. Must be one of: \"mainnet\", \"testnet\""
+  exit 1
+fi
+
+if [[ -n "${P2P_PORT}" ]]; then
+  CONFIG_P2P_PORT=${P2P_PORT}
+fi
+
+if [[ -n "${FULL_NODE_PORT}" ]]; then
+  CONFIG_FULL_NODE_PORT=${FULL_NODE_PORT}
+fi
+
+if [[ -n "${SOLIDITY_NODE_PORT}" ]]; then
+  CONFIG_SOLIDITY_NODE_PORT=${SOLIDITY_NODE_PORT}
+fi
+
+sed -i -e "s/listen.port = .*/listen.port = ${CONFIG_P2P_PORT}/g" ${CONFIG_FILE}
+sed -i -e "s/fullNodePort = .*/fullNodePort = ${CONFIG_FULL_NODE_PORT}/g" ${CONFIG_FILE}
+sed -i -e "s/solidityPort = .*/solidityPort = ${CONFIG_SOLIDITY_NODE_PORT}/g" ${CONFIG_FILE}
+
+exec java -jar /usr/local/tron/FullNode.jar -c ${CONFIG_FILE} -d /data
